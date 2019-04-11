b311a2c331de70f7eac4b0cc123145875557c69d
unknown <honorpurpose@gmail.com>
2019-04-11 16:55:28 -0600

    completed 04112019

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,18 +1,59 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import Todo from './components/todo/todo'
 import "./App.css";
 
 class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      input: '',
+      list: []
+    }
+
+    this.handleClickChange = this.handleClickChange.bind( this )
+    
+  }
+
+  handleChange(val){
+    this.setState({ input: val })
+  }
+
+  handleClickChange(){
+    if (this.state.input === '') {
+      window.alert('Please enter a task')
+    } else {
+      this.setState({
+        list: [...this.state.list, this.state.input],
+        input: ''
+      })
+    }
+  }
+
+  
+  
+  
   render() {
+    let todo = this.state.list.map((e, i) => {
+      return <Todo key={i} task={e} />
+    })
+   
     return (
+      
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className='container'>
+          <h1>Brock's To-do List</h1>
+          <div className='input'>
+            <input 
+              placeholder='Add new task'
+              onChange={ e => this.handleChange( e.target.value ) }
+              value={ this.state.input }
+            />
+            <button onClick={ this.handleClickChange }> Add </button>
+          </div>
+          {todo}
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
---
+++ app-6/src/components/todo/todo.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class Todo extends Component {
+      
+
+    render() {
+        return (
+            <p>{ this.props.task }</p>
+        )
+    }
+
+
+
+};
+
+export default Todo
